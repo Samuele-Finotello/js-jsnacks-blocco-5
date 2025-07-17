5e5776bdfa4cb14d9dde7beef7bb92fede8befb1
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-07-17 15:53:55 +0200

    [add] feat snack-8 .find

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,7 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+let studentClass = students.find((student) => student['name'] === 'Marco Lanci')
+
+console.log(studentClass['class']);
